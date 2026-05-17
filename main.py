02805e0467f93f260b28bce355fed756acf4a1ae
roblowe84 <81348921+roblowe84@users.noreply.github.com>
2026-05-17 07:04:04 +0000

    Created Table

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Hello World!")
+# Load the sqlite database
+import sqlite3
+# Creates a sqlite database called 'store'.  
+conn = sqlite3.connect('store')
+# Creates the table within the database
+conn.execute("CREATE TABLE 'pet' (name VARCHAR(20), owner VARCHAR(20), species VARCHAR(20), sex CHAR(1), checkups SMALLINT UNSIGNED, birth DATE, death DATE)")

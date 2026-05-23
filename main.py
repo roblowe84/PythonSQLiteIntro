c45a334b7e4a9e8ebf8197d487be96ef022cb328
roblowe84 <81348921+roblowe84@users.noreply.github.com>
2026-05-23 19:45:28 +0000

    Include an Update Command

--- main.py
+++ main.py
@@ -27,3 +27,15 @@ for row in cursor:
    print("birth = ", row[5])
    print("death = ", row[6], "\n")
 
+conn.execute("UPDATE pet SET species='dog' WHERE name = 'Claws';")
+conn.commit()
+
+cursor = conn.execute("SELECT name,owner,species,sex,checkups,birth,death from pet")
+for row in cursor:
+   print("name = ", row[0])
+   print("owner = ", row[1])
+   print("species = ", row[2])
+   print("sex = ", row[3])
+   print("checkups = ", row[4])
+   print("birth = ", row[5])
+   print("death = ", row[6], "\n")

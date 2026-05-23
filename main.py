e7ca00bf9a26bab487d96a42f7924bbbcf002a41
roblowe84 <81348921+roblowe84@users.noreply.github.com>
2026-05-23 19:36:17 +0000

    Insert and Select Functions

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-# Load the sqlite database
 import sqlite3
-# Creates a sqlite database called 'store'.  
 conn = sqlite3.connect('store')
-# Creates the table within the database
-conn.execute("CREATE TABLE 'pet' (name VARCHAR(20), owner VARCHAR(20), species VARCHAR(20), sex CHAR(1), checkups SMALLINT UNSIGNED, birth DATE, death DATE)")
+print ("Database has been created")
+
+conn.execute("DROP TABLE IF EXISTS pet")
+
+conn.execute("CREATE TABLE pet (name VARCHAR(20), owner VARCHAR(20), species VARCHAR(20), sex CHAR(1), checkups SMALLINT UNSIGNED, birth DATE, death DATE)")
+conn.execute("INSERT INTO pet (name,owner,species,sex,checkups,birth,death)VALUES \
+  ('Fluffy','Harold','cat','f',5,'2001-02-04','')")
+conn.execute("INSERT INTO pet (name,owner,species,sex,checkups,birth,death)VALUES \
+  ('Claws','Gwen','cat','m',2,'2000-03-17','')")
+
+conn.commit()
+print("Records created successfully")
+print("Total number of rows created :", conn.total_changes)
+
+print ("Table created successfully")
+
+cursor = conn.execute("SELECT name,owner,species,sex,checkups,birth,death from pet")
+
+for row in cursor:
+   print("name = ", row[0])
+   print("owner = ", row[1])
+   print("species = ", row[2])
+   print("sex = ", row[3])
+   print("checkups = ", row[4])
+   print("birth = ", row[5])
+   print("death = ", row[6], "\n")
+
